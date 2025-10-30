25f1b01b4370763e17adb39b1d308898462bf6be
Satvik Hatulkar <satwikhatulkar@gmail.com>
2025-10-31 00:25:45 +0530

    setup file

--- Lab1/setup.sh
+++ Lab1/setup.sh
@@ -3,6 +3,6 @@
 sudo docker pull alpine:3.18
 sudo docker build -t vu1ns0ck-image .
 
-echo "Happy Exploit ;)"
+echo "H4ppy Exploit ;)"
 
 sudo docker run -it --rm --name lab1-Vu1nS0ck -v /var/run/docker.sock:/var/run/docker.sock --user root vu1ns0ck-image bash

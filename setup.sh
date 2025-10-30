cfd725e61ac5929387e1da6742a21c096cdd9353
Satvik Hatulkar <satwikhatulkar@gmail.com>
2025-10-31 00:18:28 +0530

    Dockerfile

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker pull alpine:3.18
-sudo docker build -t vu1ns0ck-image .
-
-echo "Happy Exploit ;)"
-
-sudo docker run -it --rm --name lab1-Vu1nS0ck -v /var/run/docker.sock:/var/run/docker.sock --user root vu1ns0ck-image bash
